293ab0518a4f24c227bcc0bbec2f84c66e8d0822
ansshul10 <ansshul10@gmail.com>
2025-03-14 17:33:05 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './components/Login';
@@ -7,8 +7,28 @@ import AnonymousEntry from './components/AnonymousEntry';
 import ChatWindow from './components/ChatWindow';
 import ForgotPassword from './components/ForgotPassword';
 import ResetPassword from './components/ResetPassword';
+import io from 'socket.io-client';
+
+const socket = io(process.env.REACT_APP_SOCKET_URL || 'http://localhost:5000');
 
 function App() {
+  useEffect(() => {
+    const userId = JSON.parse(localStorage.getItem('user'))?.id || localStorage.getItem('anonymousId');
+    if (userId) socket.emit('join', userId);
+
+    socket.on('notification', ({ text }) => {
+      if (Notification.permission === 'granted') {
+        new Notification('Chatify', { body: text });
+      } else if (Notification.permission !== 'denied') {
+        Notification.requestPermission().then((permission) => {
+          if (permission === 'granted') new Notification('Chatify', { body: text });
+        });
+      }
+    });
+
+    return () => socket.off('notification');
+  }, []);
+
   return (
     <Router>
       <div className="min-h-screen bg-gradient-to-br from-gray-900 to-black text-white">
