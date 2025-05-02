06cf3d87272ddd0396dca296d94a52046712e0be
ansshul10 <ansshul10@gmail.com>
2025-05-02 22:16:27 +0530

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const AnonymousSession = require('../models/AnonymousSession');
+const User = require('../models/User');
 
 module.exports = async (req, res, next) => {
   const token = req.header('x-auth-token');
@@ -12,7 +13,12 @@ module.exports = async (req, res, next) => {
   try {
     if (token) {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const user = await User.findById(decoded.userId);
+      if (!user) {
+        return res.status(401).json({ msg: 'User not found' });
+      }
       req.user = decoded.userId;
+      req.isAdmin = user.isAdmin;
     } else if (anonymousId) {
       const session = await AnonymousSession.findOne({ anonymousId });
       if (!session) {
