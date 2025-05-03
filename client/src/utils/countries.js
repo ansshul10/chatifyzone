e4f6ff39e09afef17e7f490329eb07187038b3b6
ansshul10 <ansshul10@gmail.com>
2025-05-04 00:12:16 +0530

    Delete client/src/utils/countries.js

--- client/src/utils/countries.js
+++
@@ -1,6 +0,0 @@
-export default [
-    { code: 'US', name: 'United States' },
-    { code: 'IN', name: 'India' },
-    { code: 'GB', name: 'United Kingdom' },
-    // Add more countries as needed
-  ];
